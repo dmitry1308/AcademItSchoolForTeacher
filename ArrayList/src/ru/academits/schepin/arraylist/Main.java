7bcd1fa9f3b91ab546d2f32a72f72e3c77941281
Dmitry <bright1308@gmail.com>
2017-04-03 17:14:32 +0700

    Коммит перый задачи ArrayList

--- ArrayList/src/ru/academits/schepin/arraylist/Main.java
+++ ArrayList/src/ru/academits/schepin/arraylist/Main.java
@@ -2,7 +2,36 @@ package ru.academits.schepin.arraylist;
 
 public class Main {
     public static void main(String[] args) {
-        MyArray<Integer> numbers= new MyArray<>();
+        MyArray numbers = new MyArray();
+        numbers.add(895);
+        numbers.add(2365);
+        numbers.add(51);
+        numbers.add(25);
+        numbers.add(89);
+        numbers.set(2, 69);
+
+        System.out.println("Список = " + numbers);
+
+        int a = numbers.get(0);
+        System.out.println("Элемент под индексом 0 = " + a);
+
+        System.out.println(numbers.isEmpty());
+
+        System.out.println("Кол-во элементов в списке = " + numbers.size());
+
+
+        numbers.insert(4, 14);
+        System.out.println(numbers);
+        numbers.insert(5, 45);
+        System.out.println(numbers);
+        numbers.remove(6);
+        System.out.println(numbers);
+
+        System.out.println(numbers.size());
+
+        numbers.trimToSize();
+        System.out.println(numbers);
+
 
     }
 }
--- ArrayList/src/ru/academits/schepin/arraylist/MyArray.java
+++ ArrayList/src/ru/academits/schepin/arraylist/MyArray.java
@@ -1,8 +1,10 @@
 package ru.academits.schepin.arraylist;
 
 
+import java.util.Arrays;
+
 public class MyArray {
-    private Object[] items = new Object[10];
+    private Integer[] items = new Integer[3];
     private int length;
 
     public int size() {
@@ -11,5 +13,71 @@ public class MyArray {
 
     public boolean isEmpty() {
         return length == 0;
+    }
+
+    public Integer get(int index) {
+        if (index > items.length - 1) {
+            throw new IllegalArgumentException("Необходмо ввести индекс меньше ");
+        }
+        return items[index];
+    }
+
+    public void set(int index, Integer obj) {
+        if (index > items.length - 1) {
+            throw new IllegalArgumentException("Необходмо ввести индекс меньше ");
+        }
+        items[index] = obj;
+    }
+
+
+    public String toString() {
+        return Arrays.toString(items);
+    }
+
+    public void add(Integer obj) {
+        if (length >= items.length) {
+            increaseCapacity();
+        }
+        items[length] = obj;
+        ++length;
+    }
+
+    private void increaseCapacity() {
+        Integer[] old = items;
+        items = new Integer[old.length * 2];
+        System.arraycopy(old, 0, items, 0, old.length);
+    }
+
+    //Удаление элемента:
+    public void remove(int index) {
+        if (index >= length) {
+            throw new IllegalArgumentException("Необходмо ввести индекс меньше " + length);
+        }
+        if (index < length - 1) {
+            int numMoved = length - index - 1;
+            System.arraycopy(items, index + 1, items, index, numMoved);
+        }
+        items[--length] = null;
+    }
+
+    //Вставка элемента:
+    public void insert(int index, Integer obj) {
+        if (index > length - 1) {
+            throw new IllegalArgumentException("Необходмо ввести индекс меньше ");
+        }
+        if (length >= items.length) {
+            increaseCapacity();
+        }
+
+        int numMoved = length - index;
+        System.arraycopy(items, index, items, index + 1, numMoved);
+        items[index] = obj;
+        ++length;
+    }
+
+    public void trimToSize(){
+        Integer[] array = new Integer[length];
+        System.arraycopy(items,0,array,0,length);
+        items=array;
     }
 }
