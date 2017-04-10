6e882d825a864ab5a997d253cbef393b7b7016f6
Dmitry <bright1308@gmail.com>
2017-04-10 17:40:09 +0700

    Задача MyArrayList

--- MyArrayList/src/ru/academits/shepin/myArrayList/Main.java
+++ MyArrayList/src/ru/academits/shepin/myArrayList/Main.java
@@ -11,20 +11,20 @@ public class Main {
         for (int i = 0; i < 20; i++) {
 
             listOfString.add(String.valueOf(i));
-            System.out.printf("Size: %d  Capacity: %d%n", listOfString.getSize(), listOfString.getCapacity());
+            System.out.printf("Size: %d  Capacity: %d%n", listOfString.size(), listOfString.getCapacity());
         }
         System.out.println("**********************************************************************************");
-        for (int i = 0; i < listOfString.getSize(); i++) {
+        for (int i = 0; i < listOfString.size(); i++) {
 
             System.out.println(listOfString.get(i));
         }
         System.out.println("**********************************************************************************");
 
 
-        System.out.printf("Size: %d  Capacity: %d%n", listOfString.getSize(), listOfString.getCapacity());
+        System.out.printf("Size: %d  Capacity: %d%n", listOfString.size(), listOfString.getCapacity());
         System.out.println("**********************************************************************************");
 
-        listOfString.set(1, "Hello");
+        // listOfString.set(1, "Hello");
         System.out.println(listOfString);
         System.out.println("**********************************************************************************");
 
@@ -32,16 +32,59 @@ public class Main {
         System.out.println("Removing...");
         System.out.println("**********************************************************************************");
 
-        for (int i = 0; i < listOfString.getSize(); i++) {
+        for (int i = 0; i < listOfString.size(); i++) {
 
             System.out.printf("%s%n", listOfString.get(i));
         }
-        System.out.format("Size: %d  Capacity: %d%n", listOfString.getSize(), listOfString.getCapacity());
+        System.out.format("Size: %d  Capacity: %d%n", listOfString.size(), listOfString.getCapacity());
         listOfString.insert(1, "Bye.");
         System.out.println(listOfString);
 
         listOfString.trimToSize();
         System.out.println(listOfString);
+
+
+        MyArrayList<Integer> listOfNumbers = new MyArrayList<>(2);
+        listOfNumbers.add(4);
+        listOfNumbers.add(2);
+        listOfNumbers.add(5);
+
+        System.out.println(listOfNumbers);
+        System.out.println(listOfNumbers.size());
+
+        listOfNumbers.add(7);
+        listOfNumbers.trimToSize();
+        System.out.println(listOfNumbers);
+
+        System.out.println(listOfNumbers.indexOf(5));
+
+       // System.out.println(listOfNumbers.lastIndex(5));
+
+        //  listOfNumbers.removeElement(5);
+        System.out.println(listOfNumbers);
+        listOfNumbers.clear();
+
+        listOfNumbers.add(5);
+        listOfNumbers.add(4);
+        listOfNumbers.add(6);
+        listOfNumbers.add(6);
+        listOfNumbers.add(6);
+        listOfNumbers.add(6);
+        listOfNumbers.add(6);
+        listOfNumbers.add(6);
+        listOfNumbers.add(6);
+
+        System.out.println(listOfNumbers);
+
+
+        System.out.println(listOfNumbers.size());
+        System.out.println(listOfNumbers.contains(5));
+        System.out.println(listOfNumbers.getCapacity());
+
+
+
+
+
     }
 
 }
--- MyArrayList/src/ru/academits/shepin/myArrayList/MyArrayList.java
+++ MyArrayList/src/ru/academits/shepin/myArrayList/MyArrayList.java
@@ -1,216 +1,256 @@
 package ru.academits.shepin.myArrayList;
 
-
 import java.util.*;
 
-public class MyArrayList<T> {
-
+public class MyArrayList<T> implements List {
     private T[] innerArray;
     private int innerSize;
 
-    private static final int START_CAPACITY = 8;
+    private static final int START_CAPACITY = 10;
 
     public MyArrayList() {
-
         innerArray = (T[]) (new Object[START_CAPACITY]);
-        innerSize = 0;
     }
 
-    public MyArrayList(int iniCap) {
-
-        innerArray = (T[]) (new Object[iniCap]);
-        innerSize = 0;
+    public MyArrayList(int InitialCap) {
+        innerArray = (T[]) (new Object[InitialCap]);
     }
 
-
-    public int getSize() {
-
+    @Override
+    public int size() {
         return innerSize;
     }
 
     public int getCapacity() {
-
         return innerArray.length;
-    }
-
-    public T get(int index) {
-
-        if (index < 0 || index > innerSize - 1) {
-
-            throw new IllegalArgumentException("Введите корректный индекс вставки.");
-        }
-        return innerArray[index];
-    }
-
-    public void set(int index, T obj) {
-
-        if (index < 0 || index > innerSize - 1) {
-
-            throw new IllegalArgumentException("Введите корректный индекс вставки.");
-        }
-        innerArray[index] = obj;
     }
 
     @Override
     public String toString() {
-
-        return Arrays.toString(innerArray);
+        T[] array = Arrays.copyOf(innerArray, innerSize);
+        return Arrays.toString(array);
     }
 
-    public void ensureCapacity(int newCap) {
+    @Override
+    public boolean isEmpty() {
+        return innerSize == 0;
+    }
 
-        if (newCap > innerArray.length) {
-
-            T[] innerArray2 = (T[]) new Object[newCap];
-
-            System.arraycopy(innerArray, 0, innerArray2, 0, innerSize);
-
-            innerArray = innerArray2;
+    @Override
+    public boolean contains(Object object) {
+        for (int i = 0; i < innerSize; ++i) {
+            if (!(innerArray[i].equals(object))) {
+                continue;
+            }
+            return true;
         }
+        return false;
     }
 
-    public void add(T obj) {
+    @Override
+    // TODO: 10.04.2017
+    public Iterator iterator() {
+        return null;
+    }
 
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(innerArray, innerSize);
+    }
+
+    @Override
+    public boolean add(Object object) {
         if (innerSize == innerArray.length) {
 
             ensureCapacity(innerArray.length * 2);
         }
 
-        innerArray[innerSize] = obj;
+        innerArray[innerSize] = (T) object;
+        ++innerSize;
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object object) {
+        for (int i = 0; i < innerArray.length; ++i) {
+            if (!(innerArray[i].equals(object))) {
+                continue;
+            }
+            int numMoved = innerSize - i - 1;
+            System.arraycopy(innerArray, i + 1, innerArray, i, numMoved);
+
+            innerArray[innerSize - 1] = null;
+            innerSize--;
+
+            return true;
+        }
+        return false;
+    }
+
+    // TODO: 10.04.2017
+    @Override
+    public boolean addAll(Collection c) {
+
+        return false;
+    }
+
+    // TODO: 10.04.2017
+    @Override
+    public boolean addAll(int index, Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        if (innerArray[0] != null) {
+            for (int i = 0; i < innerSize; ++i) {
+                innerArray[i] = null;
+            }
+            innerSize = 0;
+        }
+    }
+
+    @Override
+    public Object get(int index) {
+        if (index < 0 || index > innerSize - 1) {
+            throw new IllegalStateException("Введите корректный индекс вставки.");
+        }
+        return innerArray[index];
+    }
+
+    @Override
+    public Object set(int index, Object object) {
+        if (index < 0 || index > innerSize - 1) {
+
+            throw new IllegalStateException("Введите корректный индекс вставки.");
+        }
+        innerArray[index] = (T) object;
+        return object;
+    }
+
+    public void ensureCapacity(int newCap) {
+        if (newCap > innerArray.length) {
+            T[] innerArray2 = (T[]) new Object[newCap];
+            System.arraycopy(innerArray, 0, innerArray2, 0, innerSize);
+            innerArray = innerArray2;
+        }
+    }
+
+    @Override
+    public void add(int index, Object object) {
+        if (innerSize == innerArray.length) {
+            ensureCapacity(innerArray.length * 2);
+        }
+
+        innerArray[innerSize] = (T) object;
         ++innerSize;
     }
 
-    public void remove(int index) {
-
+    @Override
+    public Object remove(int index) {
         if (index < 0 || index > innerSize - 1) {
-
             throw new IllegalArgumentException("Некорректный индекс в операции удаления!");
         }
         if (index < innerSize - 1) {
-
             int numMoved = innerSize - index - 1;
-
             System.arraycopy(innerArray, index + 1, innerArray, index, numMoved);
         }
 
         innerArray[innerSize - 1] = null;
         innerSize--;
+
+        return innerArray;
     }
 
-    public void insert(int index, T obj) {
+    @Override
+    public int indexOf(Object object) {
+        for (int i = 0; i < innerArray.length; ++i) {
+            if (!(innerArray[i].equals(object))) {
+                continue;
+            }
+            return 1;
+        }
+        return -1;
+    }
 
+    @Override
+    public int lastIndexOf(Object object) {
+        int lastIndex = -1;
+        int i;
+        for (i = 0; i < innerArray.length; ++i) {
+            if (!(innerArray[i].equals(object))) {
+                continue;
+            }
+            lastIndex = i;
+        }
+        if (lastIndex == -1) {
+            return -1;
+        } else {
+            return lastIndex;
+        }
+    }
+
+    // TODO: 10.04.2017
+    @Override
+    public ListIterator listIterator() {
+        return null;
+    }
+
+    // TODO: 10.04.2017
+    @Override
+    public ListIterator listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    // TODO: 10.04.2017
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    // TODO: 10.04.2017
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    // TODO: 10.04.2017
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return Arrays.copyOf(innerArray, innerSize);
+    }
+
+    public void insert(int index, T object) {
         if (index < 0 || index > innerSize - 1) {
-
             throw new IllegalArgumentException("Некорректный индекс в операции вставки.");
         }
         if (innerSize == innerArray.length) {
-
             ensureCapacity(innerArray.length * 2);
         }
 
         int numMoved = innerSize - index;
         System.arraycopy(innerArray, index, innerArray, index + 1, numMoved);
 
-        innerArray[index] = obj;
+        innerArray[index] = object;
         innerSize++;
     }
 
-
     public void trimToSize() {
+        if (innerArray.length > innerSize) {
 
-        T[] newArray;
-
-        newArray = Arrays.copyOf(innerArray, innerSize);
-
-        innerArray = newArray;
+            innerArray = Arrays.copyOf(innerArray, innerSize);
+        }
     }
 
-    public boolean isEmpty() {
-        return innerSize == 0;
-    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
