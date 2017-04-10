9af22dd4b207438afa330120490a6ea36ffe99c5
Dmitry <bright1308@gmail.com>
2017-04-10 17:40:55 +0700

    Коммит CFT

--- CFT/src/ru/academits/schepin/InsertionSort/manipulations/RecordToFile.java
+++ CFT/src/ru/academits/schepin/InsertionSort/manipulations/RecordToFile.java
@@ -7,11 +7,9 @@ import java.util.ArrayList;
 public class RecordToFile {
     public static void recordInFile(ArrayList<?> list, String outPutPath) {
         try (PrintWriter writer = new PrintWriter(outPutPath)) {
-            for (int i = 0; i < list.size(); ++i) {
-                writer.println(list.get(i));
-            }
+            list.forEach(writer::println);
         } catch (FileNotFoundException e) {
-            System.out.println("Список не записан в файл(проверьте путь!)");
+            System.out.println("Список не записан в файл(проверьте путь)");
         }
     }
 
