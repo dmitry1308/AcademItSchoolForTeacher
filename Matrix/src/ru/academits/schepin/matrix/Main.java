41e9a8042f72ef3ea268d6c476043c707a49c27c
Dmitry <bright1308@gmail.com>
2017-04-21 17:22:46 +0700

    Первый Коммит Matrix

--- Matrix/src/ru/academits/schepin/matrix/Main.java
+++ Matrix/src/ru/academits/schepin/matrix/Main.java
@@ -1,24 +1,172 @@
 package ru.academits.schepin.matrix;
 
+import java.util.Arrays;
+import java.util.Vector;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
+
     public static void main(String[] args) {
 
-        Matrix a = new Matrix(5, 6);
-        System.out.println(a);
+        Vector<Double> z1 = new Vector<>();
+        z1.add(2.5);
+        z1.add(2.1);
+        System.out.println("Вектор z1= " + z1);
 
-        double[][] b = {{5, 4, 8, 9, 6, 3}, {5, 8, 7, 9, 6}};
-        Matrix c = new Matrix(b);
-        System.out.println(c);
+        Vector<Double> z2 = new Vector<>();
+        z2.add(2.2);
+        z2.add(2.4);
+        System.out.println("Вектор z2= " + z2);
 
-        double[][] d = new double[2][3];
-        d[0] = new double[]{8, 548, 78};
-        d[1] = new double[]{5, 6, 9};
-        Matrix e = new Matrix(d);
-        System.out.printf("Матрица e: ( %s)%n ",e);
+        Vector[] arrayOfVector = {z1, z2};
 
-        double x = d[1][2];
-        System.out.println(x);
+        Matrix g = new Matrix(arrayOfVector);
+        System.out.println("Матрица g= " + g);
+
+        System.out.println("Умножение матрицы g на вектор z1(e = g * z1):");
+        Vector e = g.multiplyByVector(z1);
+        System.out.println("e= " + e);
+
+        System.out.println("");
+
+        Vector<Double> z3 = new Vector<>();
+        z3.add(2.5);
+        z3.add(2.1);
+        System.out.println("Вектор z3= " + z3);
+
+        Vector<Double> z4 = new Vector<>();
+        z4.add(2.5);
+        z4.add(2.1);
+        System.out.println("Вектор z4= " + z4);
+
+        Vector[] arrayOfVector1 = {z3, z4};
+
+        Matrix g1 = new Matrix(arrayOfVector1);
+        System.out.println("Матрица g1= " + g1);
+
+        System.out.println(" ");
+
+        System.out.println("Прибавление к матрице g1 к g");
+        System.out.println("Матрица g1= " + g1);
+        System.out.println("Матрица g= " + g);
+        g.add(g1);
+        System.out.println("Матрица g= " + g);
+
+        System.out.println("");
+
+        System.out.println("Сложение матриц g1 и g (g1 + g = g2)");
+        System.out.println("Матрица g1= " + g1);
+        System.out.println("Матрица g= " + g);
+        Matrix g2 = Matrix.add(g, g1);
+        System.out.println("Матрица g2= " + g2);
+
+        System.out.println("");
+
+        System.out.println("Вычитание из матрицы g1 матрицу g (g1 - g)");
+        System.out.println("Матрица g1= " + g1);
+        System.out.println("Матрица g= " + g);
+        g1.subtraction(g);
+        System.out.println("Матрица g= " + g1);
+
+        System.out.println("");
+
+        System.out.println("g - g1 = g3");
+        System.out.println("Матрица g1= " + g1);
+        System.out.println("Матрица g= " + g);
+        Matrix g3 = Matrix.subtraction(g, g1);
+        System.out.println("Матрица g3= " + g3);
+
+        System.out.println("");
+
+        System.out.println("g2 * g3 = g4");
+        System.out.println("Матрица g3= " + g3);
+        System.out.println("Матрица g2= " + g2);
+        Matrix g4 = Matrix.multiplication(g2, g3);
+        System.out.println("Матрица g4= " + g4);
 
 
+        System.out.println(" ");
+        System.out.println("Сложение матриц m1 + m2 = m3:");
+        double[][] array = {{1, 0}, {2, 3}};
+        double[][] array1 = {{0, 5}, {1, 8}};
+        Matrix m1 = new Matrix(array);
+        System.out.println("m1= " + m1);
+        Matrix m2 = new Matrix(array1);
+        System.out.println("m2= " + m2);
+
+        Matrix m3 = Matrix.add(m1, m2);
+        System.out.println("m3= " + m3);
+
+        System.out.println(" ");
+
+
+        Matrix m = new Matrix(4, 4);
+
+        for (int i = 0; i < m.getDimensions()[0]; i++) {
+
+            Vector<Double> vector = new Vector<>(4);
+
+            for (int j = 0; j < 4; j++) {
+
+                vector.add((double) ThreadLocalRandom.current().nextInt(10));
+            }
+
+            m.setRow(i, vector);
+        }
+
+
+        Matrix m4 = new Matrix(4, 4);
+
+        for (int i = 0; i < m.getDimensions()[0]; i++) {
+
+            Vector<Double> vector = new Vector<>(4);
+
+            for (int j = 0; j < 4; j++) {
+
+                vector.add((double) ThreadLocalRandom.current().nextInt(10));
+            }
+
+            m4.setRow(i, vector);
+        }
+
+        System.out.println("Сложение матриц m + m4 = m5:");
+        System.out.println("m= " + m);
+        System.out.println("m4= " + m4);
+
+        Matrix m5 = Matrix.add(m, m4);
+
+        System.out.println("m5= " + m5);
+
+        System.out.println("\n" + "Определитель матрицы m = " + m.determinant());
+
+
+        System.out.println("\n" +"Умножение матрицы m на скаляр:");
+        System.out.println("m= " + m);
+        m.multiplyByScalar(3);
+        System.out.println("m * 3 = " + m);
+
+        System.out.println("\n" +"Размер матицы m:");
+        System.out.println("m = " + m);
+        int []size = m.getDimensions();
+        System.out.println("Размер m:"+ Arrays.toString(size));
+
+        System.out.println("\n" +"Получение вектора столбца матрицы m5 по индексу 2:");
+        System.out.println("m5= " + m5);
+        double [] column=m5.getColumnCopy(2);
+        System.out.println("Вектор столбец по индексу 2:"+ Arrays.toString(column));
+
+        System.out.println("\n" +"Транспонирование матрицы g:");
+        System.out.println("g= " + g);
+        g.transpose();
+        System.out.println("транспонированная матрица g  = " + g);
+
+        System.out.println("\n" +"Задание вектора строки матрицы g по индексу 1 вектором z4:");
+        System.out.println("g= " + g);
+        System.out.println("z4= " + z4);
+        g.setRow(1,z4);
+        System.out.println("g"+ g);
     }
+
+
+
 }
--- Matrix/src/ru/academits/schepin/matrix/Matrix.java
+++ Matrix/src/ru/academits/schepin/matrix/Matrix.java
@@ -3,52 +3,340 @@ package ru.academits.schepin.matrix;
 import java.util.Vector;
 
 public class Matrix {
-    private double[][] array;
+
+    private Vector[] data;
 
     public Matrix(int n, int m) {
-        if (n < 0 && m < 0) {
-            throw new IllegalArgumentException("Некорректный индекс");
-        }
-        array = new double[n][m];
-    }
+        data = new Vector[n];
 
-    public Matrix(double[][] vector) {
-        this.array = vector;
-    }
+        for (int i = 0; i < n; i++) {
 
-    public Matrix(Vector[] a) {
-        //// TODO:
-    }
-
-    public Matrix(Matrix m) {
-        double[][] array1 = new double[m.getSize()][];
-        for (int i=0;i<m.getSize();++i){
-
-        }
-    }
-
-
-    public int getSize(){
-        return array.length;
-    }
-
-    public String toString() {
-        StringBuilder a = new StringBuilder();
-        for (int i = 0; i < array.length; ++i) {
-            a.append("[");
-            a.append(i + 1);
-            a.append("]");
-            a.append("[");
-            a.append(array.length);
-            a.append("]");
-            a.append("-->  ");
-            for (int j = 0; j < array[i].length; ++j) {
-                a.append(array[i][j]);
-                a.append(", ");
+            data[i] = new Vector(m);
+            for (int j = 0; j < m; j++) {
+                data[i].add(0);
             }
-            a.deleteCharAt(a.length() - 2);
-            a.append("  ");
         }
-        return a.toString();
     }
+
+    public Matrix(Matrix matrix) {
+        data = new Vector[matrix.data.length];
+
+        for (int i = 0; i < data.length; i++) {
+            data[i] = new Vector(matrix.data[0].size());
+
+            for (int j = 0; j < matrix.data[i].size(); j++) {
+                data[i].add(matrix.data[i].get(j));
+            }
+        }
+    }
+
+    public Matrix(double[][] m) {
+        data = new Vector[m.length];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = new Vector(m[0].length);
+
+            for (int j = 0; j < m[i].length; j++) {
+                data[i].add(m[i][j]);
+            }
+        }
+    }
+
+    public Matrix(Vector[] rows) {
+        int columns = rows[0].size();
+        data = new Vector[rows. length];
+
+        for (int i = 0; i < data.length; i++) {
+            data[i] = new Vector(columns);
+
+            for (int j = 0; j < columns; j++) {
+                data[i].add((rows[i].get(j)));
+            }
+        }
+    }
+
+    public int[] getDimensions() {
+        int[] dims = {data.length, data[0].size()};
+        return dims;
+    }
+
+    public Vector getRowCopy(int index) {
+        if (index < 0 || index >= data.length) {
+            throw new IllegalArgumentException("Индекс строки некорректен!");
+        }
+
+        Vector newRow = new Vector(data[index]);
+
+        return newRow;
+    }
+
+    public void setRow(int index, Vector newRow) {
+        if (index < 0 || index >= data.length) {
+            throw new IllegalArgumentException("Индекс строки некорректен!");
+        }
+
+        if (newRow.size() != data[0].size()) {
+            throw new IllegalArgumentException("Размер новой строки некорректен!");
+        }
+
+        data[index] = newRow;
+    }
+
+    public double[] getColumnCopy(int index) {
+        if (index < 0 || index >= data[0].size()) {
+            throw new IllegalArgumentException("Индекс столбца некорректен!");
+        }
+
+        double[] newColumn = new double[data.length];
+
+        for (int i = 0; i < newColumn.length; i++) {
+            newColumn[i] = (Double) data[i].get(index);
+        }
+        return newColumn;
+    }
+
+
+    private double get(int i, int j) {
+        return (Double) data[i].get(j);
+    }
+
+
+    private void set(int i, int j, double d) {
+        data[i].set(j, d);
+    }
+
+
+    public void transpose() {
+        if (data.length != data[0].size()) {
+            throw new IllegalStateException("Транспонирование неквадратных матриц не определено!");
+        }
+
+        for (int i = 1; i < data.length; i++) {
+
+            for (int j = 0; j < i; j++) {
+                double t = get(i, j);
+                set(i, j, get(j, i));
+                set(j, i, t);
+            }
+        }
+    }
+
+    public void multiplyByScalar(double k) {
+        for (int i = 0; i < data.length; i++) {
+
+            for (int j = 0; j < data[0].size(); j++) {
+                set(i, j, get(i, j) * k);
+            }
+        }
+    }
+
+    public double determinant() {
+        if (data.length != data[0].size()) {
+            throw new IllegalStateException("Определитель неквадратных матриц не существует!");
+        }
+
+
+        if (data.length == 1) {
+            return get(0, 0);
+        }
+
+        if (data.length == 2) {
+            return get(0, 0) * get(1, 1) - get(1, 0) * get(0, 1);
+        }
+
+        if (data.length == 3) {
+            return get(0, 0) * get(1, 1) * get(2, 2) + get(0, 1) * get(1, 2) * get(2, 0) + get(1, 0) * get(2, 1) * get(0, 2) -
+                    get(2, 0) * get(1, 1) * get(0, 2) - get(0, 0) * get(2, 1) * get(1, 2) - get(1, 0) * get(0, 1) * get(2, 2);
+        } else {
+
+            double result = 0;
+            for (int i = 0; i < data.length; i++) {
+
+                Matrix m = new Matrix(data.length - 1, data.length - 1);
+                int step = 0;
+
+                for (int j = 0; j < data.length; j++) {
+
+                    if (j == i) {
+                        step = 1;
+                        continue;
+                    }
+
+                    for (int k = 1; k < data.length; k++) {
+                        m.set(j - step, k - 1, get(j, k));
+                    }
+                }
+                double d = m.determinant();
+
+                result += get(i, 0) * d * Math.pow(-1, i);
+            }
+            return result;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("{");
+        for (int i = 0; i < data.length; i++) {
+
+            sb.append("{");
+            for (int j = 0; j < data[0].size(); j++) {
+                sb.append(data[i].get(j));
+                if (j < data[0].size() - 1) sb.append(" ");
+            }
+
+            sb.append("}");
+            if (i < data.length - 1) sb.append(",");
+        }
+        sb.append("}");
+
+        return sb.toString();
+    }
+
+    public Vector<Double> multiplyByVector(Vector<Double> vector) {
+        if (vector.size() != data[0].size()) {
+
+            throw new IllegalArgumentException("Длина вектора должна совпадать с длиной строки матрицы!");
+        }
+
+        Vector<Double> result = new Vector<>(data.length);
+        for (int i = 0; i < data.length; i++) {
+            double sum = 0;
+
+            for (int j = 0; j < data[i].size(); j++) {
+                sum += get(i, j) * vector.get(j);
+            }
+            result.add(sum);
+        }
+        return result;
+    }
+
+    public void add(Matrix m) {
+
+        if (m.data.length != data.length || m.data[0].size() != data[0].size()) {
+
+            throw new IllegalArgumentException("Сложение матриц разного размера запрещено!");
+        }
+        for (int i = 0; i < data.length; i++) {
+
+            for (int j = 0; j < data[i].size(); j++) {
+
+                set(i, j, get(i, j) + m.get(i, j));
+            }
+        }
+    }
+
+
+    public void subtraction(Matrix m) {
+
+        if (m.data.length != data.length || m.data[0].size() != data[0].size()) {
+
+            throw new IllegalArgumentException("Сложение матриц разного размера запрещено!");
+        }
+        for (int i = 0; i < data.length; i++) {
+
+            for (int j = 0; j < data[i].size(); j++) {
+
+                set(i, j, get(i, j) - m.get(i, j));
+            }
+        }
+    }
+
+
+    public static Matrix add(Matrix a, Matrix b) {
+
+        if (a.data.length != b.data.length || a.data[0].size() != b.data[0].size()) {
+
+            throw new IllegalArgumentException("Сложение матриц разного размера запрещено!");
+        }
+
+        Matrix c = new Matrix(a.data.length, a.data[0].size());
+
+        for (int i = 0; i < a.data.length; i++) {
+
+            for (int j = 0; j < a.data[i].size(); j++) {
+
+                c.set(i, j, a.get(i, j) + b.get(i, j));
+            }
+        }
+
+        return c;
+    }
+
+
+    public static Matrix subtraction(Matrix a, Matrix b) {
+
+        if (a.data.length != b.data.length || a.data[0].size() != b.data[0].size()) {
+
+            throw new IllegalArgumentException("Сложение матриц разного размера запрещено!");
+        }
+
+        Matrix c = new Matrix(a.data.length, a.data[0].size());
+
+        for (int i = 0; i < a.data.length; i++) {
+
+            for (int j = 0; j < a.data[i].size(); j++) {
+
+                c.set(i, j, a.get(i, j) - b.get(i, j));
+            }
+        }
+
+        return c;
+    }
+
+
+    public static Matrix multiplication(Matrix a, Matrix b) {
+
+        if (a.data.length != b.data.length || a.data[0].size() != b.data[0].size()) {
+
+            throw new IllegalArgumentException("Сложение матриц разного размера запрещено!");
+        }
+
+        Matrix c = new Matrix(a.data.length, a.data[0].size());
+
+        for (int i = 0; i < a.data.length; i++) {
+
+            for (int j = 0; j < a.data[i].size(); j++) {
+
+                c.set(i, j, a.get(i, j) * b.get(i, j));
+            }
+        }
+
+        return c;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
